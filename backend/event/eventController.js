162abd18cd3fa9c6c5bb1c5cf8906da88c65bcc4
Fangda Zuo <fangda.zuo@tum.de>
2017-07-15 15:19:49 +0200

    Added event filters

--- backend/event/eventController.js
+++ backend/event/eventController.js
@@ -30,9 +30,7 @@ exports.getEvents = function (req, res) {
 };
 
 exports.getEventsByFilter = function (req, res) {
-    console.log(req.params.date);
-    console.log(new Date(req.params.date));
-    Event.find({ city: req.params.city, guestCount: { $gte: req.params.guestCount }, time: new Date(req.params.date) }, function (err, events) {
+    Event.find({ isOpen: true, city: req.params.city, guestCount: { $gte: req.params.guestCount }, time: new Date(req.params.date) }, function (err, events) {
         if (err) {
             res.status(400).send(err);
             return;
@@ -40,10 +38,31 @@ exports.getEventsByFilter = function (req, res) {
         res.json(events);
     });
 };
+
+exports.getEventsByUser = function (req, res) {
+    Event.find({ user: req.params.user }, function (err, events) {
+        if (err) {
+            res.status(400).send(err);
+            return;
+        }
+        res.json(events);
+    });
+};
+
+exports.getEventsByGuest = function (req, res) {
+    Event.find({ guest: req.params.guest }, function (err, events) {
+        if (err) {
+            res.status(400).send(err);
+            return;
+        }
+        res.json(events);
+    });
+};
+
 // Create endpoint /api/events/:event_id for GET
 exports.getEvent = function (req, res) {
     // Use the Event model to find a specific event
-    Event.findById(req.params.event_id, function (err, event) {
+    Event.find({ _id: req.params.event_id }, function (err, event) {
         if (err) {
             res.status(400).send(err)
             return;
